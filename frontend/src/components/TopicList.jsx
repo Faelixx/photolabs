00e8ee29202cacb460b617af00a80e484b7b885d
Faelixx <sasha.taylor7@gmail.com>
2024-02-29 10:23:08 -0700

    Fix: Code cleanup, remove mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import topics from "mocks/topics";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((el) => (
+      {topics.map((el) => (
         <TopicListItem
           key={el.id}
           title={el.title}/>
