21c0df63ff4be30bc0561d52cc4df2aaebca8a90
Faelixx <sasha.taylor7@gmail.com>
2024-02-29 10:00:41 -0700

    Feat: Add Navigation Bar style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
+
+import TopicList from './TopicList';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span><TopicList /></span>
+      <span>❤️</span>
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
